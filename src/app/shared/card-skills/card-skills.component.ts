c05505956b66403c0f265c080f9f6b3bb6fc24b1
Ingrid Oliveira <ngrid.vianna1@outlook.com>
2025-08-04 23:18:24 -0300

    :construction: feat: updated about-me component, card-skills component, footer component and styles with new content and design

--- src/app/shared/card-skills/card-skills.component.ts
+++ src/app/shared/card-skills/card-skills.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-skills.component.scss']
 })
 export class CardSkillsComponent {
-
+  skills = [
+    {
+      name: 'HTML',
+      icon: '../../../assets/HTML-icon.png',
+    },
+    {
+      name: 'CSS',
+      icon: '../../../assets/CSS-icon.png',
+    },
+    {
+      name: 'JavaScript',
+      icon: '../../../assets/JavaScript-icon.png',
+    },
+    {
+      name: 'Angular',
+      icon: '../../../assets/Angular-icon.png',
+    },
+    {
+      name: 'TypeScript',
+      icon: '../../../assets/typescript-icon.png',
+    },
+    {
+      name: 'Git',
+      icon: '../../../assets/GitHub-icon.png',
+    }
+  ]
 }
--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
-
+  linkedinUrl = 'https://www.linkedin.com/in/ingrid-vianna/';
+  email = 'viannaingrid856@gmail.com';
+  githubUrl = 'https://github.com/viannaingrid';
 }
